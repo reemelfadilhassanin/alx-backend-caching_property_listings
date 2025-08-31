33b2af856967700f40d21bb8f67b7fa6e2912fdb
root <root@Reem>
2025-08-31 09:45:13 +0200

    Cache Property List View

--- properties/apps.py
+++ properties/apps.py
@@ -3,3 +3,12 @@ from django.apps import AppConfig
 
 class PropertiesConfig(AppConfig):
     name = 'properties'
+from django.apps import AppConfig
+
+
+class PropertiesConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'properties'
+
+    def ready(self):
+        import properties.signals
